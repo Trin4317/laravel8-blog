ce7221b770f494b463062a56240118e3bf7273e4
Trin1437 <eg.artourbabaev@gmail.com>
2022-04-09 17:07:43 +0900

    switch from php to blade syntax

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,12 +5,12 @@
 
     @foreach ($posts as $post)
         <article>
-            <a href="/post/<?= $post->slug ?>">
-                <h1><?= $post->title; ?></h1>
+            <a href="/post/{{ $post->slug }}">
+                <h1>{{ $post->title }}</h1>
             </a>
 
             <div>
-                <?= $post->excerpt ?>
+                {{ $post->excerpt }}
             </div>
         </article>
     @endforeach
