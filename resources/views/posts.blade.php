0a8edaeb319ba9c5ca851ee35249e11707db51e6
Trin1437 <eg.artourbabaev@gmail.com>
2022-04-09 17:55:24 +0900

    using blade component  to create layout

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,6 +1,4 @@
-@extends('layout')
-
-@section('content')
+<x-layout>
     @foreach ($posts as $post)
     <article>
         <a href="/post/{{ $post->slug }}">
@@ -12,4 +10,4 @@
         </div>
     </article>
     @endforeach
-@endsection
+</x-layout>
