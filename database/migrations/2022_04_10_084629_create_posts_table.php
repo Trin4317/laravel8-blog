2b6728487d730b97053568bbaefc9259d2569b43
Trin1437 <eg.artourbabaev@gmail.com>
2022-04-10 20:05:04 +0900

    resolve conflict before merging

--- database/migrations/2022_04_10_084629_create_posts_table.php
+++ database/migrations/2022_04_10_084629_create_posts_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('slug')->unique();
             $table->foreignId('category_id');
-            $table->string('slug');
             $table->string('title');
             $table->text('excerpt');
             $table->text('body');
