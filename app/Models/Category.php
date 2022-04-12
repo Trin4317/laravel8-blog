0fd2542ed6fa74dcc054247a9c8f168111e6a35a
Trin1437 <eg.artourbabaev@gmail.com>
2022-04-12 21:30:46 +0900

    add Eloquen relationship to Category class

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function posts()
+    {
+        // hasOne, hasMany, belongsTo, belongsToMany
+        // does a category has many posts? true
+        return $this->hasMany(Post::class);
+    }
 }
