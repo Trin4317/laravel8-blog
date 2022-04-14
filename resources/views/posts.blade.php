868be7c00bd79e0215ef4c18daf6f676dd1dc7a6
Trin1437 <eg.artourbabaev@gmail.com>
2022-04-14 23:36:54 +0900

    update posts view to handle the case where there is only 1 or no post

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,12 +2,18 @@
     @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card :post="$posts[0]"/>
+        @if ($posts->count())
+            <x-post-featured-card :post="$posts[0]"/>
 
-        <div class="lg:grid lg:grid-cols-2">
-            @foreach ($posts->skip(1) as $post)
-                <x-post-card :post="$post"/>
-            @endforeach
-        </div>
+            @if ($posts->count() > 1)
+                <div class="lg:grid lg:grid-cols-2">
+                    @foreach ($posts->skip(1) as $post)
+                        <x-post-card :post="$post"/>
+                    @endforeach
+                </div>
+            @endif
+        @else
+            <p class="text-center">No posts yet. Please check back later</p>
+        @endif
     </main>
 </x-layout>
