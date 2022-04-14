d10824cc56615f58adb8600cd011389aedc13f10
Trin1437 <eg.artourbabaev@gmail.com>
2022-04-15 00:03:15 +0900

    adjust posts view to show older posts in smaller grid

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,9 +6,15 @@
             <x-post-featured-card :post="$posts[0]"/>
 
             @if ($posts->count() > 1)
-                <div class="lg:grid lg:grid-cols-2">
+                {{-- make a gird of 6 columns --}}
+                <div class="lg:grid lg:grid-cols-6">
                     @foreach ($posts->skip(1) as $post)
-                        <x-post-card :post="$post" class="bg-red-500"/>
+                        {{-- for the first 2 posts, span 3 columns --}}
+                        {{-- span 2 columns for the rest --}}
+                        <x-post-card
+                            :post="$post"
+                            class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2' }}"
+                        />
                     @endforeach
                 </div>
             @endif
