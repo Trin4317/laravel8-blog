6fa466129a650fe49c162e512f1f5eda2f72dd54
Trin1437 <eg.artourbabaev@gmail.com>
2022-04-17 21:53:52 +0900

    make category dropdown menu include an all category option if currentCategory is set

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -32,6 +32,12 @@
                 </button>
 
                 <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl z-50" style="display:none">
+                    @if (isset($currentCategory))
+                        <a href="/"
+                        class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white"
+                        >All</a>
+                    @endif
+
                     @foreach ( $categories as $category)
                         <a href="/categories/{{ $category->slug }}"
                             class="
