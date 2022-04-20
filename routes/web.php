e6002206f61cf5c59018ac074fbc1c78e5f3ec8d
Trin1437 <eg.artourbabaev@gmail.com>
2022-04-20 22:27:49 +0900

    remove category route

--- routes/web.php
+++ routes/web.php
@@ -21,20 +21,6 @@ Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('post/{post}', [PostController::class, 'show']);
 
-Route::get('categories/{category}', function (Category $category) {
-    // using route-model binding
-    // binding a route key {category} to underlying Eloquent Category model
-    // using posts property to fetch all data from a single category
-
-    // solving n+1 when the data isnt fetched from the Class but Collection
-    // using load method to eager load the relationship between Post model and others
-    return view('posts', [
-        'posts' => $category->posts,
-        'categories' => Category::all(),
-        'currentCategory' => $category
-    ]);
-})->name('category');
-
 Route::get('authors/{author}', function (User $author) {
     // using route-model binding
     // binding a route key {author} to underlying Eloquent User model
