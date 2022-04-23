0e73c579ad959d1d5741f6fccd22dd0f7dd69196
Trin1437 <eg.artourbabaev@gmail.com>
2022-04-23 23:06:05 +0900

    create register.create view

---
+++ resources/views/register/create.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <body style="font-family: Open Sans, sans-serif">
+        <section class="px-6 py-8">
+            <h1>Hello Friends</h1>
+        </section>
+    </body>
+</x-layout>
