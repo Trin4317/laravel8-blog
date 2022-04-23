ef7c0e3ef2afb8e3dbb8f59e7483a8e2e052d904
Trin1437 <eg.artourbabaev@gmail.com>
2022-04-23 23:54:14 +0900

    update store method to create new user in database

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
@@ -13,12 +14,15 @@ class RegisterController extends Controller
 
     public function store()
     {
-
-        request()->validate([
+        $attributes = request()->validate([
             'name' => 'required|max:255',
             'username' => 'required|max:255|min:3',
             'email' => 'required|email|max:255',
             'password' => 'required|max:255|min:7' // or ['required', 'min:7', 'max:255']
         ]);
+
+        User::create($attributes);
+
+        return redirect('/');
     }
 }
