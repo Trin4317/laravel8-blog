bd47ad5cc4333a18c7c4affb2473185dfa0fa9c8
Trin1437 <eg.artourbabaev@gmail.com>
2022-04-27 00:53:42 +0900

    create flash view component

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -54,13 +54,5 @@
             </div>
         </footer>
     </section>
-
-    @if (session()->has('success'))
-        <div x-data="{ show: true }"
-            x-init="setTimeout(() => show = false, 4000)"
-            x-show="show"
-            class="fixed bg-blue-500 text-white py-2 px-4 rounded-xl bottom-3 right-3 test-sm">
-            <p>{{ session()->get('success') }}</p>
-        </div>
-    @endif
+    <x-flash />
 </body>
