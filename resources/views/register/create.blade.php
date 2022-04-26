5cc983be848f35d3447d6b3182a6607f4e84c9e6
Trin1437 <eg.artourbabaev@gmail.com>
2022-04-27 00:37:08 +0900

    add another section to display all validation error messages

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -87,6 +87,15 @@
                             Submit
                         </button>
                     </div>
+
+                    {{-- display all the errors --}}
+                    @if ($errors->any())
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li class="text-red-500 text-xs">{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
                 </form>
             </main>
         </section>
