29995e9fe53967448951d7a24502c5691dcd0597
Trin1437 <eg.artourbabaev@gmail.com>
2022-04-29 17:46:01 +0900

    include route for login request

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,8 @@ Route::get('register', [RegisterController::class, 'create'])->middleware('guest
 
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
 
+Route::get('login', [SessionController::class, 'create'])->middleware('guest');
+
+Route::post('sessions', [SessionController::class, 'store'])->middleware('guest');
+
 Route::post('logout', [SessionController::class, 'destroy'])->middleware('auth');
