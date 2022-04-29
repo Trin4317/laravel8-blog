15ed54779d8ccae8bde022dff6af645386fa9f49
Trin1437 <eg.artourbabaev@gmail.com>
2022-04-29 17:19:17 +0900

    add create method in Session controller

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class SessionController extends Controller
 {
+    public function create()
+    {
+        return view('session.create');
+    }
+
     public function destroy()
     {
         auth()->logout();
