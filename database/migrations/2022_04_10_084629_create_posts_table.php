e1bcac79d30fc3d82a0c65af3dfc084688ba67a7
Trin1437 <eg.artourbabaev@gmail.com>
2022-04-30 17:18:19 +0900

    update foreign key constraint for Post model

--- database/migrations/2022_04_10_084629_create_posts_table.php
+++ database/migrations/2022_04_10_084629_create_posts_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('slug')->unique();
             $table->foreignId('category_id');
-            $table->foreignId('user_id');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete(); // remove the posts from deleted user
             $table->string('title');
             $table->text('excerpt');
             $table->text('body');
