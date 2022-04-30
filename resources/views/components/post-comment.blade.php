b7e2089b01c6fab658e6d97ba5dc5a0078db618f
Trin1437 <eg.artourbabaev@gmail.com>
2022-04-30 18:01:41 +0900

    update show view to display comments dynamically

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,3 +1,5 @@
+@props(['comment'])
+
 <article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
     <div class="flex-shrink-0">
         <img src="https://i.pravatar.cc/100" alt="" width="60" height="60" class="rounded-xl">
@@ -5,15 +7,15 @@
 
     <div>
         <header class="mb-4">
-            <h3 class="font-bold">John Doe</h3>
+            <h3 class="font-bold">{{ $comment->author->name }}</h3>
 
             <p class="text-xs">
-                Posted
-                <time>8 months ago</time>
+                Posted on
+                <time>{{ $comment->created_at }}</time>
             </p>
         </header>
         <p>
-            This is the comment.
+            {{ $comment->body }}
         </p>
     </div>
 </article>
