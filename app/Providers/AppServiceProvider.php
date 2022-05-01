72ecef8f09b3e92a193391247dc317e0bd945004
Trin1437 <eg.artourbabaev@gmail.com>
2022-05-01 14:52:01 +0900

    add comment for AppServiceProvider about disabling mass assignable restriction

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Database\Eloquent\Model;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -28,5 +29,9 @@ class AppServiceProvider extends ServiceProvider
         // switch between which pagination style we want to use
         // default is useTailwind()
         // Paginator::useBootstrap();
+
+        // disable all mass assignable restrictions
+        // which means we don't have to provide $fillable or $guarded property for each Model
+        // Model::unguard();
     }
 }
