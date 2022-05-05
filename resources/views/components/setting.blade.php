ebdb15c8485734b179eea33b35107569ba0f5d2b
Trin1437 <eg.artourbabaev@gmail.com>
2022-05-05 22:28:48 +0900

    extract page setting to dedicated view component

---
+++ resources/views/components/setting.blade.php
@@ -0,0 +1,10 @@
+@props(['heading'])
+
+<section class="px-6 py-8 max-w-md mx-auto">
+    <h1 class="text-lg font-bold mb-4">
+        {{ $heading }}
+    </h1>
+    <x-panel>
+        {{ $slot }}
+    </x-panel>
+</section>
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,40 +1,35 @@
 <x-layout>
-    <section class="px-6 py-8 max-w-md mx-auto">
-        <h1 class="text-lg font-bold mb-4">
-            Publish New Post
-        </h1>
-        <x-panel>
-            <form method="POST" action="/admin/posts" enctype="multipart/form-data">
-                @csrf
+    <x-setting heading="Publish New Post">
+        <form method="POST" action="/admin/posts" enctype="multipart/form-data">
+            @csrf
 
-                <x-form.input name="title"></x-form.input>
+            <x-form.input name="title"></x-form.input>
 
-                <x-form.input name="slug"></x-form.input>
+            <x-form.input name="slug"></x-form.input>
 
-                <x-form.input name="thumbnail" type="file"></x-form.input>
+            <x-form.input name="thumbnail" type="file"></x-form.input>
 
-                <x-form.textarea name="excerpt"></x-form.textarea>
+            <x-form.textarea name="excerpt"></x-form.textarea>
 
-                <x-form.textarea name="body"></x-form.textarea>
+            <x-form.textarea name="body"></x-form.textarea>
 
-                <x-form.field>
-                    <x-form.label name="category_id">Category</x-form.label>
+            <x-form.field>
+                <x-form.label name="category_id">Category</x-form.label>
 
-                    <select name="category_id" id="category_id">
-                        @foreach (\App\Models\Category::all() as $category)
-                            <option
-                                value="{{ $category->id }}"
-                                {{ old('category_id') == $category->id ? 'selected' : '' }}
-                            >{{ $category->name }}</option>
-                        @endforeach
-                    </select>
+                <select name="category_id" id="category_id">
+                    @foreach (\App\Models\Category::all() as $category)
+                        <option
+                            value="{{ $category->id }}"
+                            {{ old('category_id') == $category->id ? 'selected' : '' }}
+                        >{{ $category->name }}</option>
+                    @endforeach
+                </select>
 
-                    <x-form.error name="category_id"></x-form.error>
-                </x-form.field>
+                <x-form.error name="category_id"></x-form.error>
+            </x-form.field>
 
-                <x-form.button>Publish</x-form.button>
+            <x-form.button>Publish</x-form.button>
 
-            </form>
-        </x-panel>
-    </section>
+        </form>
+    </x-setting>
 </x-layout>
