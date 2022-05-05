525c9bc85663d5ffda96665f7ece6919e48617c4
Trin1437 <eg.artourbabaev@gmail.com>
2022-05-05 18:01:38 +0900

    add a thumbnail property for Post migration file

--- database/migrations/2022_04_10_084629_create_posts_table.php
+++ database/migrations/2022_04_10_084629_create_posts_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->foreignId('category_id');
             $table->foreignId('user_id')->constrained()->cascadeOnDelete(); // remove the posts from deleted user
             $table->string('title');
+            $table->string('thumbnail')->nullable();
             $table->text('excerpt');
             $table->text('body');
             $table->timestamps();
