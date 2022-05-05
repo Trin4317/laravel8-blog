beefe5ec5ac7ce33274c29ec0d91a943090a7248
Trin1437 <eg.artourbabaev@gmail.com>
2022-05-05 22:47:40 +0900

    refactor sesson.create view with panel components

--- resources/views/session/create.blade.php
+++ resources/views/session/create.blade.php
@@ -1,17 +1,19 @@
 <x-layout>
     <body style="font-family: Open Sans, sans-serif">
         <section class="px-6 py-8">
-            <main class="max-w-lg mx-auto mt-10 bg-gray-100 border border-gray-200 p-6 rounded-xl">
-                <h1 class="text-center font-bold text-xl">Log In!</h1>
+            <main class="max-w-lg mx-auto mt-10">
+                <x-panel>
+                    <h1 class="text-center font-bold text-xl">Log In!</h1>
 
-                <form method="POST" action="/sessions" class="mt-10">
-                    @csrf
-                    <x-form.input name="email" type="email"></x-form.input>
+                    <form method="POST" action="/sessions" class="mt-10">
+                        @csrf
+                        <x-form.input name="email" type="email"></x-form.input>
 
-                    <x-form.input name="password" type="password"></x-form.input>
+                        <x-form.input name="password" type="password"></x-form.input>
 
-                    <x-form.button>Log In</x-form.button>
-                </form>
+                        <x-form.button>Log In</x-form.button>
+                    </form>
+                </x-panel>
             </main>
         </section>
     </body>
