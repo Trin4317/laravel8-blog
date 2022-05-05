9478ca0d34038ab700e78edf9a586b6f60e970b9
Trin1437 <eg.artourbabaev@gmail.com>
2022-05-05 22:55:51 +0900

    update form input component to accept all attributes without explicit defining

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,14 +1,17 @@
-@props(['name', 'type' => 'text'])
+@props(['name'])
 
 <x-form.field>
     <x-form.label name="{{ $name }}"></x-form.label>
 
     <input class="border border-gray-200 p-2 w-full rounded"
-        type="{{ $type }}"
+        {{-- default type for input is text --}}
         name="{{ $name }}"
         id="{{ $name }}"
         value="{{ old($name) }}"
-        required>
+        required
+        {{-- accept all attributes without explicitly defining it in @props --}}
+        {{ $attributes }}
+    >
 
     <x-form.error name="{{ $name }}"></x-form.error>
 </x-form.label>
