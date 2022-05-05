b2fc3e7550dd463d8275915c767db2653e1986da
Trin1437 <eg.artourbabaev@gmail.com>
2022-05-05 19:10:34 +0900

    extract form label to dedicated view component

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,10 +1,7 @@
 @props(['name', 'type' => 'text'])
 
 <div class="mb-6">
-    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-        for="{{ $name }}">
-        {{ ucwords($name) }}
-    </label>
+    <x-form.label name="{{ $name }}"></x-form.label>
 
     <input class="border border-gray-400 p-2 w-full"
         type="{{ $type }}"
--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -1,10 +1,7 @@
 @props(['name'])
 
 <div class="mb-6">
-    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-        for="{{ $name }}">
-        {{ ucwords($name) }}
-    </label>
+    <x-form.label name="{{ $name }}"></x-form.label>
 
     <textarea class="border border-gray-400 p-2 w-full"
         name="{{ $name }}"
