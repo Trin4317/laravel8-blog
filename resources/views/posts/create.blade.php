4122ebd404b895d80dd61ab4dc041e5802679ce3
Trin1437 <eg.artourbabaev@gmail.com>
2022-05-05 18:59:07 +0900

    extract form input to dedicated view component

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,58 +6,12 @@
         <x-panel>
             <form method="POST" action="/admin/posts" class="mt-1" enctype="multipart/form-data">
                 @csrf
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                        for="title">
-                        Title
-                    </label>
 
-                    <input class="border border-gray-400 p-2 w-full"
-                        type="text"
-                        name="title"
-                        id="title"
-                        value="{{ old('title') }}"
-                        required>
+                <x-form.input name="title"></x-form.input>
 
-                    @error('title')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
+                <x-form.input name="slug"></x-form.input>
 
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                        for="slug">
-                        Slug
-                    </label>
-
-                    <input class="border border-gray-400 p-2 w-full"
-                        type="text"
-                        name="slug"
-                        id="slug"
-                        value="{{ old('slug') }}"
-                        required>
-
-                    @error('slug')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                        for="thumbnail">
-                        Thumbnail
-                    </label>
-
-                    <input class="border border-gray-400 p-2 w-full"
-                        type="file"
-                        name="thumbnail"
-                        id="thumbnail"
-                        required>
-
-                    @error('slug')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
+                <x-form.input name="thumbnail" type="file"></x-form.input>
 
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
