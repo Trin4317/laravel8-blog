f7f3bb35c4aa15aff7806f88a60a380ef8c3ba73
Trin1437 <eg.artourbabaev@gmail.com>
2022-05-07 02:21:49 +0900

    include header for posts list in dashboard

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,30 +1,30 @@
 <x-layout>
     <x-setting heading="Manage Posts">
+        {{-- TODO! fix CSS max width problem --}}
         <div class="flex flex-col">
             <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                     <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                         <table class="min-w-full divide-y divide-gray-200">
-                            {{-- <thead class="bg-gray-50">
-                            <tr>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Name
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Title
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Status
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Role
-                                </th>
-                                <th scope="col" class="relative px-6 py-3">
-                                <span class="sr-only">Edit</span>
-                                </th>
-                            </tr>
-                            </thead> --}}
-                            {{-- TODO! fix CSS problem --}}
+                            <thead class="bg-gray-50">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Title
+                                    </th>
+                                    <th scope="col" class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Status
+                                    </th>
+                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Author
+                                    </th>
+                                    <th scope="col" colspan="2" class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Action
+                                    </th>
+                                    {{-- <th scope="col" class="relative px-6 py-3">
+                                    <span class="sr-only">Edit</span>
+                                    </th> --}}
+                                </tr>
+                            </thead>
                             <tbody class="bg-white divide-y divide-gray-200">
                                 @foreach ($posts as $post)
                                     <tr>
