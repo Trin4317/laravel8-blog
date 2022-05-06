ef6769ff9171fdc97bb97f6c12086b1727bb10b2
Trin1437 <eg.artourbabaev@gmail.com>
2022-05-06 18:47:58 +0900

    move create and store method from Post controller to AdminPost controller

--- app/Http/Controllers/AdminPostController.php
+++ app/Http/Controllers/AdminPostController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\Category;
+use Illuminate\Validation\Rule;
 
 class AdminPostController extends Controller
 {
@@ -12,5 +14,31 @@ class AdminPostController extends Controller
         return view('admin.posts.index', [
             'posts' => Post::latest()->paginate(10)
         ]);
+    }
+
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'title' => 'required',
+            'slug' => ['required', Rule::unique('posts', 'slug')],
+            'thumbnail' => 'required|image',
+            'excerpt' => 'required',
+            'body' => 'required',
+            'category_id' => ['required', Rule::exists('categories', 'id')]
+        ]);
+
+        // $attributes['user_id'] = auth()->id();
+        // Post::create($attributes);
+
+        // return the path where the file was stored
+        $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails');
+        auth()->user()->posts()->create($attributes);
+
+        return redirect('/');
     }
 }
