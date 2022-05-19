369173b534e12ca7f9c93041779bfba819795a74
Trin1437 <eg.artourbabaev@gmail.com>
2022-05-19 23:04:05 +0900

    update form.button view component with custom class depends on option value

--- resources/views/components/form/button.blade.php
+++ resources/views/components/form/button.blade.php
@@ -1,6 +1,17 @@
+@php
+    $classes = "text-white uppercase font-semibold text-xs py-2 px-10 mt-2 rounded-2xl";
+
+    if (!isset($attributes['option'])) {
+        $classes .= " bg-blue-500 hover:bg-blue-600";
+    }
+    if ($attributes['option'] === 'draft') {
+        $classes .= " bg-yellow-500 hover:bg-yellow-600";
+    }
+@endphp
+
 <x-form.field>
     <button type="submit"
-            class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 mt-2 rounded-2xl hover:bg-blue-600"
+            {{ $attributes->merge([ 'class' => $classes ]) }}
     >
         {{ $slot }}
     </button>
