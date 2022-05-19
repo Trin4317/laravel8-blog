b747903fb6602643e7ee6806f41960c113f3d49d
Trin1437 <eg.artourbabaev@gmail.com>
2022-05-19 21:48:42 +0900

    update homepage to only show published posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,8 +12,10 @@ class PostController extends Controller
     public function index()
     {
         return view('posts.index', [
-            'posts' => Post::latest()->filter(
-                request(['search', 'category', 'author'])
+            'posts' => Post::where('status', 'PUBLISHED')
+                ->latest()
+                ->filter(
+                    request(['search', 'category', 'author'])
                 )->paginate(3)->withQueryString()
         ]);
     }
