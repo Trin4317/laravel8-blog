313a1c2adb0c68d016f7f5aeee3a0661ed4f6c97
Trin1437 <eg.artourbabaev@gmail.com>
2022-06-07 00:36:12 +0900

    update css for bookmark index view

--- resources/views/users/bookmark.blade.php
+++ resources/views/users/bookmark.blade.php
@@ -3,12 +3,13 @@
         <main class="mx-auto">
             <ul>
                 @foreach ($bookmarks as $bookmark)
-                    <li class="flex justify-between px-24 mb-3">
-                            <a href="/post/{{ $bookmark->slug }}" class="hover:text-blue-500">{{ $bookmark->title }}</a>
-                            <form method="POST" action="/profile/unbookmark/{{ $bookmark->id }}">
-                                @csrf
-                                <button type="submit" class="w-6 h-6 rounded-full bg-red-500 text-white">✕</button>
-                            </form>
+                    <li class="flex justify-between items-baseline px-4 mb-3">
+                        <a href="/post/{{ $bookmark->slug }}" class="hover:text-blue-500 min-w-">{{ $bookmark->title }}</a>
+                        <span class="text-xs ml-auto mr-2"><time>{{ $bookmark->pivot->created_at->diffForHumans(null, false, true) }}</time></span>
+                        <form method="POST" action="/profile/unbookmark/{{ $bookmark->id }}">
+                            @csrf
+                            <button type="submit" class="w-6 h-6 rounded-full bg-red-500 text-white">✕</button>
+                        </form>
                     </li>
                 @endforeach
             </ul>
