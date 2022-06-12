50d4789d8139bdcd53ac620e2e2923c123df8cd7
Trin1437 <eg.artourbabaev@gmail.com>
2022-06-12 16:59:02 +0900

    create a corresponding listener for UserCreated event

---
+++ app/Listeners/SendRegistrationEmail.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\UserCreated;
+use App\Mail\WelcomeMail;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Mail;
+
+class SendRegistrationEmail
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \App\Events\UserCreated  $event
+     * @return void
+     */
+    public function handle(UserCreated $event)
+    {
+        // send an email to newly registered user
+        Mail::to($event->user->email)->send(new WelcomeMail($event->user));
+    }
+}
