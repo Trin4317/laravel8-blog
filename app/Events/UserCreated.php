9088138d0e7a6a6e0037479d7b8ca4a5cf77ec5a
Trin1437 <eg.artourbabaev@gmail.com>
2022-06-12 16:58:00 +0900

    create UserCreated event

---
+++ app/Events/UserCreated.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\User;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class UserCreated
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * The user instance.
+     *
+     * @var App\Models\User
+     */
+    public $user;
+
+    /**
+     * Create a new event instance.
+     * Note: when there is new item in User table, Eloquent will dispatch an event
+     * and this event class is a container for that User instance
+     *
+     * @param App\Models\User $user
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
